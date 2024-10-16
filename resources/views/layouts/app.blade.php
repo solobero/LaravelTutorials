41d561b6d73ca78b16b75d00303a561f70b81184
Vero <vzapatav@eafit.edu.co>
2024-10-15 19:52:27 -0500

    Layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,13 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="annonymous" />
     <link href="{{asset('/css/app.css')}}" rel="stylesheet" />
-    <title>@yield('title','Fishes Info')</title>
+    <title>@yield('title','Online Store')</title>
 </head>
 <body>
     <!--Header-->
     <nav class="navbar navbar-expand-lg navbar-dark bg-secondary py-4">
         <div class="container">
-            <a class="navbar-brand" href="{{route('home.index')}}">Fishes Info</a>
+            <a class="navbar-brand" href="{{route('home.index')}}">Online Store</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" 
             aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -19,10 +19,18 @@
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav ms-auto">
                     <a class="nav-link active" href="{{route('home.index')}}">Home</a>
-                    <a class="nav-link active" href="{{route('fish.register')}}">Fishes Register</a>
-                    <a class="nav-link active" href="{{route('fish.index')}}">Fishes</a>
-                    <a class="nav-link active" href="{{route('fish.statistic')}}">Fishes Stadistics</a>
+                    <a class="nav-link active" href="{{route('home.about')}}">About</a>
                     <div class="vr bg-white mx-2 d-none d-lg-block"></div>
+                    @guest
+                    <a class="nav-link active" href="{{ route('login') }}">Login</a>
+                    <a class="nav-link active" href="{{ route('register') }}">Register</a>
+                    @else
+                    <form id="logout" action="{{ route('logout') }}" method="POST">
+                    <a role="button" class="nav-link active"
+                    onclick="document.getElementById('logout').submit();">Logout</a>
+                    @csrf
+                    </form>
+                    @endguest 
                 </div>
             </div>
         </div>   
@@ -45,8 +53,8 @@
         <div class="container">
             <small>
                 Copyright - <a class="text-reset fw-bold text-decoration-none" target="_blank"
-                href="#">
-                Verónica Zapata
+                href="https://twitter.com/danielgarax">
+                Daniel Correa
                 </a>
             </small>
         </div>
